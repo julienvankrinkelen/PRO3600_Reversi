fdc12f80e8e66d4aab0a93d5107521c319ab6b7b
Valérien BRAYE <valerien.braye@telecom-sudparis.eu>
2022-03-21 14:34:16 +0100

    Fix des méthodes du moteur de jeu (GameState.validPositions(), GameState.sandwicheck() + traduction des commentaires de SimpleView en anglais

--- src/application/Move.java
+++ src/application/Move.java
@@ -30,24 +30,19 @@ public class Move {
 		return false; //if no sandwich can be made from this position, then the move is not legal
 	}
 	
-	int disksFlipped() {
-		//TODO if needed
-		return 0;
-	}
-	
-    int flipDiscs() {
+    int flipDiscs(boolean flip) { // return the nummber of disks that can be flipped by playing this and if (flip), flips them
         Position[] dirs = new Position[] {new Position(-1, -1), new Position(-1, 0), new Position(-1, 1), new Position(0, -1), new Position(0, 1), new Position(1, -1), new Position(1, 0), new Position(1, 1)}; // list of all possible directions
         int res=0; // sum of every disc flipped
         for (int i=0; i<dirs.length; i++) { // for every direction
             int count_i = this.currentGameState.sandwicheck(this, dirs[i]); // number of discs that can be flipped in said direction
             res=res+count_i; // adds number of flipped discs
-            for (int j=1; j<count_i +1; j++) {
-                int x = this.position.x + (dirs[i].x)*j;
-                int y = this.position.y + (dirs[i].y)*j; // coordinates of the disc to flip
-                this.currentGameState.grid[x][y] = this.player; // flips the disc
+	        for (int j=1; j<count_i +1; j++) {
+	            int x = this.position.x + (dirs[i].x)*j;
+	            int y = this.position.y + (dirs[i].y)*j; // coordinates of the disc to flip
+	            this.currentGameState.grid[x][y] = this.player; // flips the disc
             }
         }
-        if (res>0) {
+        if (res>0 && flip) {
             this.currentGameState.grid[this.position.x][this.position.y] = this.player;
         }
         return res;
